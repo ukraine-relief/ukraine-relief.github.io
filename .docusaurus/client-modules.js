b572e7218f1ba43e956df9ce8370c8b7406c934b
Bruno Felix <bruno.felix@waterdog.mobi>
2022-03-07 23:47:53 +0100

    Set up docusaurus

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/home/felix/dev/projects/ukraine/ukraine-relief.github.io/node_modules/infima/dist/css/default/default.css'),
+  require('/home/felix/dev/projects/ukraine/ukraine-relief.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/home/felix/dev/projects/ukraine/ukraine-relief.github.io/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
+  require('/home/felix/dev/projects/ukraine/ukraine-relief.github.io/src/css/custom.css'),
+];
